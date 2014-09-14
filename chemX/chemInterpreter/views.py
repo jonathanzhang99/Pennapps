66493efcb71905a53e84322db759622a5eb5afdd
woodenchopsticks <jonathan.z99@gmail.com>
2014-09-14 01:04:56 -0400

    chem

--- chemX/chemInterpreter/views.py
+++ chemX/chemInterpreter/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse	
 from django.template import loader, RequestContext
 from chemspipy import ChemSpider
 
 # Create your views here.
 security_token = "132fb6c6-5179-4ce4-9b4f-2e9ee518d143"
-CS = ChemSpider()
+CS = ChemSpider(security_token)
 def index(req):
+
+	if req.method == "POST":
+		response = HttpResponse
+		body = req.body
+		chemType = req.chemType
+		if chemType == 'element':
+
+
 	
 	template = loader.get_template('chemInterpreter/index.html')
 	return render(req, 'chemInterpreter/index.html')
 
-	if (req.method == "POST"){
 
-	}
+
+
+	
