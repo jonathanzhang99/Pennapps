f739a0061e94a778802b708a5fd94c0c927a9f0a
woodenchopsticks <jonathan.z99@gmail.com>
2014-09-14 01:28:25 -0400

    chemspider tests

--- chemX/chemInterpreter/views.py
+++ chemX/chemInterpreter/views.py
@@ -5,7 +5,7 @@ from chemspipy import ChemSpider
 
 # Create your views here.
 security_token = "132fb6c6-5179-4ce4-9b4f-2e9ee518d143"
-CS = ChemSpider(security_token)
+
 def index(req):
 
 	if req.method == "POST":
@@ -13,13 +13,18 @@ def index(req):
 		body = req.body
 		chemType = req.chemType
 		if chemType == 'element':
+			CS = ChemSpider(security_token)
+			current_chem_symbol = CS.search(req.)
 
 
 	
 	template = loader.get_template('chemInterpreter/index.html')
 	return render(req, 'chemInterpreter/index.html')
 
+def SMILEtoLATEX(molecularFormula){
 
+	
+}
 
 
 	
