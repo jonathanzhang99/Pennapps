8e67fa2fc1fd04eb420e65741ab738490364f437
woodenchopsticks <jonathan.z99@gmail.com>
2014-09-14 02:57:47 -0400

    python code
    asd

--- chemX/chemInterpreter/views.py
+++ chemX/chemInterpreter/views.py
@@ -5,9 +5,31 @@ from chemspipy import ChemSpider
 
 # Create your views here.
 security_token = "132fb6c6-5179-4ce4-9b4f-2e9ee518d143"
-# def textToLatex(name, charge, number, neutrons, atomicMass){
-# 	return none
-# }
+
+def textToLatex(name, charge=None, number=None, neutrons=None, atomicMass=None):
+	latexstr = '/ce{%s}'
+	latexname = name
+	if number != None and number > 1:
+		latexname + str(number)
+	if charge != None and charge != 0:
+		latexname + "^"
+		sign = ''
+		if charge > 0:
+			sign = '+'
+		else:
+			sign = '-'
+
+		if abs(charge) != 1:
+			sign + str(abs(charge))
+		latexname = format(latexname + "%s", sigh)
+	if neutrons != None:
+		latexname = "_{" + str(neutrons) + "}" + latexname
+	if atomicMass != None:
+		latexname = "^{" + str(atomicMass) + "}" + latexname
+
+	return format(latexstr, latexname)
+
+
 def index(req):
 
 	if req.method == "POST":
@@ -19,7 +41,7 @@ def index(req):
 			current_chem_symbol = CS.search(body)
 
 		elif body.chemType == 'periodicTable':
-			return JSONResponsetextToTLatex(body.name, body.charge, body.number, body.neutrons, body.atomicMass)
+			return JSONResponsetextToLatex(body.name, body.charge, body.number, body.neutrons, body.atomicMass)
 		else:
 			return
 
