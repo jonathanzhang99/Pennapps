5c35684c21dd2a298e5a352569686f13c67683ee
Ken Noh <kennoh415@gmail.com>
2014-09-14 04:54:49 -0400

    ?

--- chemX/chemInterpreter/views.py
+++ chemX/chemInterpreter/views.py
@@ -44,7 +44,7 @@ def index(req):
 
 		elif body.chemType == 'periodicTable':
 			return JSONResponse({"latex": str(textToLatex(body.name, body.charge, body.number, body.neutrons, body.atomicMass))})
-		if body.
+			
 		else:
 			return
 	
