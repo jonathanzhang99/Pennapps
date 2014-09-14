b3ba7589856446c0d249e4fa39b769e83eb19864
woodenchopsticks <jonathan.z99@gmail.com>
2014-09-14 01:52:27 -0400

    fixes

--- chemX/chemInterpreter/views.py
+++ chemX/chemInterpreter/views.py
@@ -5,7 +5,9 @@ from chemspipy import ChemSpider
 
 # Create your views here.
 security_token = "132fb6c6-5179-4ce4-9b4f-2e9ee518d143"
-
+# def textToLatex(name, charge, number, neutrons, atomicMass){
+# 	return none
+# }
 def index(req):
 
 	if req.method == "POST":
@@ -15,13 +17,18 @@ def index(req):
 		if body.chemType == 'element':
 			CS = ChemSpider(security_token)
 			current_chem_symbol = CS.search(body)
-		elif body.chemType == 
 
+		elif body.chemType == 'periodicTable':
+			return JSONResponsetextToTLatex(body.name, body.charge, body.number, body.neutrons, body.atomicMass)
+		else:
+			return
 
 
 	
-	template = loader.get_template('chemInterpreter/index.html')
-	return render(req, 'chemInterpreter/index.html')
+	else:
+		template = loader.get_template('chemInterpreter/index.html')
+		return render(req, 'chemInterpreter/index.html')
+
 
 # def SMILEtoLATEX(molecularFormula){
 # 	break
