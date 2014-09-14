d1977c18000a7425c6454a0a5807b1e959914c4e
woodenchopsticks <jonathan.z99@gmail.com>
2014-09-14 01:03:52 -0400

    beginning of Chemspider

--- chemX/chemInterpreter/views.py
+++ chemX/chemInterpreter/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, RequestContext
+from chemspipy import ChemSpider
+
 # Create your views here.
+security_token = "132fb6c6-5179-4ce4-9b4f-2e9ee518d143"
+CS = ChemSpider()
 def index(req):
 	
 	template = loader.get_template('chemInterpreter/index.html')
 	return render(req, 'chemInterpreter/index.html')
+
+	if (req.method == "POST"){
+
+	}
